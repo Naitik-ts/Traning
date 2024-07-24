4a5da3306936bc289730735edb896396dd57fe33
Naitik Bagiya <naitik.bagiya@talentsystems.com>
2024-07-24 14:32:18 +0530

    new updates

---
+++ arrayMethods.js
@@ -0,0 +1,22 @@
+// const numbers=[1,2,3,4,5];
+// numbers.forEach((n,i)=>{
+//     console.log("number => ",n,"index is => ",i)
+// })
+// console.log("Number is => ",numbers);
+
+// const shift_num=numbers.shift();
+// console.log("Shift are => ",shift_num);
+
+// const unshift_num=numbers.unshift();
+// console.log("Unshift are => ",unshift_num);
+
+
+
+//map
+// const double=numbers.map((item)=>item**2);
+
+//const filter=numbers.filter((item)=>item%2===0);
+// console.log("Filtered array is => ",filter);
+
+//array.reduce method
+
---
+++ stringMethods.js
@@ -0,0 +1,26 @@
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
+// String indexOf()
+// String lastIndexOf()
+
+// const word = 'hello ';
+// const sentence = word.repeat(5) + '!';
+// console.log(sentence); 
+
+// const greeting = 'Hello, Naitik!';
+// const newGreeting = greeting.replace('Naitik', 'Raj');
+// console.log(newGreeting); 
+
+// const story = 'The cat chased the cat.';
+// const newStory = story.replaceAll('cat', 'dog');
+// console.log(newStory); 
+
+// const fruits = 'apple=banana=orange';
+// const fruitArray = fruits.split('=');
+// console.log(fruitArray.join(",")); 
+
+// const phrase = 'The quick brown fox jumps over the lazy dog.';
+// const wordIndex = phrase.indexOf('quick');
+// console.log(wordIndex);
